4c573232202c95fefcc173b9ee7ca4eb9fcb803c
Pol Llovet <pol.llovet@gmail.com>
2010-02-10 14:04:45 -0700

    some update? no idea.

--- lab3-css/spec/lab3_spec.rb
+++ lab3-css/spec/lab3_spec.rb
@@ -1,5 +1,22 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "Lab3 Project" do
-  
+  # 1 point. All level 1 header text, <h1>, should appear as sans-serif text that is 50 pixels high, have a background color of rgb(255,255,150) and be centered.
+
+  # 2 points. All level 2 header text, <h2>, should appear as serif text that is 25 pixels high, have a background color of #0000ff, be italicized, be in the small-caps font variant and have a 5 pixel spacing between each character.
+
+  # 1 point. The background image takes up the whole page and is a repeated version of the pattern listed in the Useful Resources section below. The background image is found in a subdirectory entitled images of the main directory entitled lab3.
+
+  # 1 point. The spacing between each word that appears after a bullet in a list is 5 pixels.
+
+  # 1 point. All paragraph text, <p>, has a line above it and a font weight of 900.
+
+  # 1 point. When the cursor is placed above a hypertext link, the text of the link should turn red, be in all uppercase letters, not be underlined, and have a background color of rgb(255,255,150).
+
+  # 1 point. Any text that is bold, <b>, in the original html document should be blue in color.
+
+  # 1 point. The lab3.css file validates.
+
+  # 1 point. The expected directory structure is correct.
+  # NOTE: this is inconsistent with the lab submission instructions
 end
