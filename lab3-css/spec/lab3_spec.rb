ede57b615dfd9933309c88d96abdfce0e9ee135e
Pol Llovet <pol.llovet@gmail.com>
2010-02-24 10:09:25 -0700

    added stub tests to lab3

--- lab3-css/spec/lab3_spec.rb
+++ lab3-css/spec/lab3_spec.rb
@@ -1,17 +1,46 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "Lab3 Project" do
-  # 1 point. All level 1 header text, <h1>, should appear as sans-serif text that is 50 pixels high, have a background color of rgb(255,255,150) and be centered.
+  
+  before(:all) do
+    @doc = open("lab3/index.html") { |f| Hpricot(f) }
+  end
+  
+  # 1 point. All level 1 header text, <h1>, should appear as sans-serif text that is 50 pixels high,
+  # have a background color of rgb(255,255,150) and be centered.
+  describe "header 1 text" do
+    it "should be san-serif text"
+    it "should be 50 pixels high"
+    it "should have a background color of rgb(255,255,150)"
+    it "should be centered"
+  end
 
-  # 2 points. All level 2 header text, <h2>, should appear as serif text that is 25 pixels high, have a background color of #0000ff, be italicized, be in the small-caps font variant and have a 5 pixel spacing between each character.
+  # 2 points. All level 2 header text, <h2>, should appear as serif text that is 25 pixels high, 
+  # have a background color of #0000ff, be italicized, be in the small-caps font variant and have a 
+  # 5 pixel spacing between each character.
+  describe "header 2 text" do
+    it "should appear as serif text"
+    it "should be 25 pixels high"
+    it "should have a background color of #0000ff"
+    it "should be italicized"
+    it "should be small-caps font variant"
+    it "should have a 5 pixel spacing between each character"
+  end
 
-  # 1 point. The background image takes up the whole page and is a repeated version of the pattern listed in the Useful Resources section below. The background image is found in a subdirectory entitled images of the main directory entitled lab3.
+  # 1 point. The background image takes up the whole page and is a repeated version of the pattern 
+  # listed in the Useful Resources section below. The background image is found in a subdirectory 
+  # entitled images of the main directory entitled lab3.
+  describe "the background image" do
+    it "should exist as images/pattern_096.gif"
+    it "should be repeated (tiled)"
+  end
 
   # 1 point. The spacing between each word that appears after a bullet in a list is 5 pixels.
 
   # 1 point. All paragraph text, <p>, has a line above it and a font weight of 900.
 
-  # 1 point. When the cursor is placed above a hypertext link, the text of the link should turn red, be in all uppercase letters, not be underlined, and have a background color of rgb(255,255,150).
+  # 1 point. When the cursor is placed above a hypertext link, the text of the link should turn red, 
+  # be in all uppercase letters, not be underlined, and have a background color of rgb(255,255,150).
 
   # 1 point. Any text that is bold, <b>, in the original html document should be blue in color.
 
