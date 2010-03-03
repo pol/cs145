8fc289703edd74fcaf660865114246769bf988df
Pol Llovet <pol.llovet@gmail.com>
2010-03-03 12:24:10 -0700

    adding lab7 directory

---
+++ lab7-css/spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'spec'
+require 'hpricot'
+require 'w3c_validators'
+require 'css_parser'
+require 'spec/autorun'
+require 'open-uri'
+
+include W3CValidators
+include CssParser
