c6602e9604fc5bf7482162515ebbdb56364b4cc3
Pol Llovet <pol.llovet@gmail.com>
2010-04-22 13:19:47 -0600

    updated the index javascript a lot

--- wiley-books/content/js/rockpaperscissors.js
+++ wiley-books/content/js/rockpaperscissors.js
@@ -1,12 +1,12 @@
 // This requires jQuery
 var rps = ["rock","paper","scissors"];
+var msg = ["win","lose","draw"];
 
 $(document).ready(function() {
   //TODO: There has to be a better way to do this
   $("#"+rps[0]).click(function(){ play(rps[0]); });
   $("#"+rps[1]).click(function(){ play(rps[1]); });
   $("#"+rps[2]).click(function(){ play(rps[2]); });
-  // $("#front-logo").hide()
 });
 
 function play(choice){
@@ -14,7 +14,15 @@ function play(choice){
   var result = rps[rand];
   var consequence = "draw";
   var msg = '';
+  resetButtons();
 
+  // for(i in rps){
+  //   if(rps[i] != choice){
+  //     $("#"+rps[i]).fadeOut("slow");
+  //   }
+  // }
+  // $("#game-buttons").addClass("final");
+  // $("#game-buttons").removeClass("initial");
   $("#"+choice).addClass("pick");
   $("#"+choice).removeClass("default");
   $("#result").addClass(result);
@@ -44,20 +52,25 @@ function play(choice){
   switch(consequence){
     case 'win':
       msg = "You win."
-      $('#front-logo').show();
+      $('#win').fadeIn("slow");
       break;
     case 'lose':
-      msg = "You lose."
+      $('#lose').fadeIn("slow");
       break;
     case 'draw':
-      msg = "It's a tie."
+      $('#draw').fadeIn("slow");
       break;
   }
-  $("#msg").text(msg);
+  $('#front-logo').fadeIn("slow");
 }
 
 function resetButtons(){
   for(i in rps){
     $('#'+rps[i]).removeClass('pick').addClass('default');
+    $("#result").removeClass(rps[i])
+  }
+  for(i in msg){
+    $('#'+msg[i]).slideUp().fadeOut('slow');
+    $("#result").removeClass(msg[i])
   }
 }
